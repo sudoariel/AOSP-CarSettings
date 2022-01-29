06ac9e01c61ff4624111676f7f5b9a12c5703255
Ariel Andrade <lima.ariel97@gmail.com>
2022-01-29 18:48:46 -0300

    checking intent actions for time changes

--- src/com/android/car/settings/info/DateViewPreferenceController.java
+++ src/com/android/car/settings/info/DateViewPreferenceController.java
@@ -1,16 +1,49 @@
 package com.android.car.settings.info;
 
 import android.car.drivingstate.CarUxRestrictions;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import androidx.preference.Preference;
 import com.android.car.settings.common.FragmentController;
 import com.android.car.settings.common.PreferenceController;
 
 public class DateViewPreferenceController extends PreferenceController<Preference> {
 
+    private final IntentFilter mIntentFilter;
+    private Integer mCounter = 0;
+    private final BroadcastReceiver mTimeChangeReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mCounter++;
+            refreshUi();
+        }
+    };
+
+   
+
     public DateViewPreferenceController(Context context, String preferenceKey,
             FragmentController fragmentController, CarUxRestrictions uxRestrictions) {
         super(context, preferenceKey, fragmentController, uxRestrictions);
+
+        // Filtering intents related to possible date changes.
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(Intent.ACTION_TIME_CHANGED);
+        mIntentFilter.addAction(Intent.ACTION_TIME_TICK);
+        mIntentFilter.addAction(Intent.ACTION_TIMEZONE_CHANGED);
+    }
+
+    @Override
+    protected void onStartInternal() {
+        // Restarting counter
+        mCounter = 0;
+        getContext().registerReceiver(mTimeChangeReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onStopInternal() {
+        getContext().unregisterReceiver(mTimeChangeReceiver);
     }
     
     @Override
@@ -20,6 +53,6 @@ public class DateViewPreferenceController extends PreferenceController<Preferenc
 
     @Override
     public void updateState(Preference preference) {
-        preference.setSummary("Testing summary for date.");
+        preference.setSummary(mCounter.toString());
     }
 }
